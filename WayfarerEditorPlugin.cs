7064d35fea5c29256a63e989e82f785bfcf89d49
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-02 12:34:37 +0300

    Initial commit of the new Wayfarer.Editor plugin

---
+++ WayfarerEditorPlugin.cs
@@ -0,0 +1,36 @@
+﻿#if TOOLS
+
+using Godot;
+using Wayfarer.Core.Systems;
+
+namespace Wayfarer.Editor
+{
+    public class WayfarerEditorPlugin : EditorPlugin
+    {
+        public EditorInterface EditorInterface => GetEditorInterface();
+        
+        public override void _EnterTree()
+        {
+            AddEditorSystems();
+        }
+
+        public override void _ExitTree()
+        {
+            
+        }
+
+        private void AddEditorSystems()
+        {
+            Iterator iterator = new Iterator();
+            iterator.Name = "EditorIterator";
+            EditorInterface.GetBaseControl().AddChild(iterator);
+        }
+
+        private void RemoveEditorSystems()
+        {
+            
+        }
+    }
+}
+
+#endif
