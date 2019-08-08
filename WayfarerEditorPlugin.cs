9052176b589ea34117870d333039b71c222d024d
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-08 15:43:59 +0300

    We lost the new WayfarerEditorPlugin when we merged the dev branch to master and checked it out, luckily I was able to recover some version of it that seems to work. There might be some things missing that could cause problems... so this would be the perfect place to start if something goes horribly wrong

--- WayfarerEditorPlugin.cs
+++ WayfarerEditorPlugin.cs
@@ -1,34 +1,119 @@
 ﻿#if TOOLS
 
+using System;
+using System.Linq;
 using Godot;
 using Wayfarer.Core.Systems;
+using Wayfarer.Core.Systems.Managers;
+using Wayfarer.ModuleSystem;
+using Wayfarer.Utils.Debug;
+using Wayfarer.Utils.Files;
+using Wayfarer.Utils.Helpers;
+using Object = Godot.Object;
 
 namespace Wayfarer.Editor
 {
-    public class WayfarerEditorPlugin : EditorPlugin
+    [Tool]
+    public class WayfarerEditorPlugin : WayfarerModule
     {
-        public EditorInterface EditorInterface => GetEditorInterface();
+        private static WayfarerEditorPlugin _instance;
+        private Iterator _iterator;
+        private MouseManager _mouseManager;
         
-        public override void _EnterTree()
+        public static WayfarerEditorPlugin Instance => _instance;
+        public EditorInterface EditorInterface => GetEditorInterface();
+        public Iterator Iterator => GetIterator();
+        public MouseManager MouseManager => GetMouseManager();
+        
+        public override void _EnterTreeSafe()
         {
-            AddEditorSystems();
-        }
-
-        public override void _ExitTree()
-        {
+            _instance = this;
             
+            try
+            {
+                AddEditorSystems();
+            }
+            catch (Exception e)
+            {
+                Log.Wayfarer.Error("Couldn't AddEditorSystems", e, true);
+            }
+        }
+        
+        public override void _ExitTreeSafe()
+        {
+            try
+            {
+                //RemoveEditorSystems();
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't remove old Editor systems", e, true);
+            }
         }
 
         private void AddEditorSystems()
         {
-            Iterator iterator = new Iterator();
-            iterator.Name = "EditorIterator";
-            EditorInterface.GetBaseControl().AddChild(iterator);
+            GetIterator();
+            GetMouseManager();
         }
 
-        private void RemoveEditorSystems()
+        public MouseManager GetMouseManager()
         {
+            if (!IsInstanceValid(_mouseManager) || _mouseManager == null)
+            {
+                MouseManager mouseManager = new MouseManager() { Name = "MouseManager" };
+                try
+                {
+                    AddChild(mouseManager);
+                    _mouseManager = mouseManager;
+                }
+                catch (Exception e)
+                {
+                    Log.Wf.EditorError("Couldn't add MouseManager to the base node of editor...?", e, true);
+                }
+            }
             
+            return _mouseManager;
+        }
+
+        public Iterator GetIterator()
+        {
+            if (!IsInstanceValid(_iterator) || _iterator == null)
+            {
+                Iterator iterator = new Iterator { Name = "EditorIterator" };
+                try
+                {
+                    AddChild(iterator);
+                    _iterator = iterator;
+                }
+                catch (Exception e)
+                {
+                    Log.Wf.EditorError("Couldn't add EditorIterator to the base node of editor...?", e, true);
+                }
+            }
+            
+            return _iterator;
+        }
+
+        public void RemoveEditorSystems()
+        {
+            try
+            {
+                _iterator.QueueFree();
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't remove the old iterator", e, true);
+            }
+            
+            try
+            {
+                _mouseManager.QueueFree();
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't remove the old MouseManager", e, true);
+            }
         }
     }
 }
